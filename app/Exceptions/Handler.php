484a8a2cf496c78a84e07ec601eec4100a5faa8f
Şafak Veziran <safak.veziran@gmail.com>
2019-12-09 21:05:54 +0300

    Exception message handler upgrade...

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -8,6 +8,12 @@ use Illuminate\Auth\Access\AuthorizationException;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Laravel\Lumen\Exceptions\Handler as ExceptionHandler;
 use Symfony\Component\HttpKernel\Exception\HttpException;
+use HttpResponseException;
+use Illuminate\Http\Response;
+use Symfony\Component\HttpKernel\Exception\MethodNotAllowedHttpException;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Throwable;
+
 
 class Handler extends ExceptionHandler
 {
@@ -46,9 +52,31 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Exception $exception)
     {
+        $status = Response::HTTP_INTERNAL_SERVER_ERROR;
+        if ($exception instanceof HttpResponseException) {
+            $status = Response::HTTP_INTERNAL_SERVER_ERROR;
+        } elseif ($exception instanceof MethodNotAllowedHttpException) {
+            $status = Response::HTTP_METHOD_NOT_ALLOWED;
+            $exception = new MethodNotAllowedHttpException([], 'HTTP_METHOD_NOT_ALLOWED', $exception);
+        } elseif ($exception instanceof NotFoundHttpException) {
+            $status = Response::HTTP_NOT_FOUND;
+            $exception = new NotFoundHttpException('HTTP_NOT_FOUND', $exception);
+        } elseif ($exception instanceof AuthorizationException) {
+            $status = Response::HTTP_FORBIDDEN;
+            $exception = new AuthorizationException('HTTP_FORBIDDEN', $status);
+        } elseif ($exception instanceof \Dotenv\Exception\ValidationException && $exception->getMessage()) {
+            $status = Response::HTTP_BAD_REQUEST;
+            $exception = new \Dotenv\Exception\ValidationException('HTTP_BAD_REQUEST', $status, $exception);
+        } elseif ($exception instanceof Throwable) {
+            $status = Response::HTTP_BAD_REQUEST;
+            $exception = new \Dotenv\Exception\ValidationException($exception->getMessage(), $status, $exception);
+        } elseif ($exception) {
+            $status = Response::HTTP_INTERNAL_SERVER_ERROR;
+            $exception = new HttpResponseException($status, 'HTTP_INTERNAL_SERVER_ERROR');
+        }
         return response()->json([
             'success' => false,
-            'message' => $exception->getMessage(),
-        ], $exception->getCode());
+            'message' => $exception->getMessage()
+        ], $status);
     }
 }
